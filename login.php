938266ad854eca0ec717166639c3105c9b1a8736
hardzal <rizaldoeta98@gmail.com>
2019-11-30 11:32:30 +0700

    Initial commit

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+require_once './database.php';
+
+session_start();
+$message = '';
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+}
+
+if (isset($_POST['login'])) {
+    $query = "SELECT * FROM login WHERE username = :username";
+
+    $statement = $connect->prepare($query);
+    $statement->bindParam(":username", $_POST['username']);
+    $statement->execute();
+
+    $count = $statement->rowCount();
+    if ($count) {
+        $result = $statement->fetchAll();
+        foreach ($result as $row) :
+            if (password_verify($_POST['password'], $row['password'])) {
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['username'] = $row['username'];
+
+                $sub_query = "INSERT INTO login_details(user_id) VALUES('" . $row['user_id'] . "')";
+
+                $statement = $connect->prepare($sub_query);
+                $statement->execute();
+
+                $_SESSION['login_details_id'] = $connect->lastInsertId();
+                header("Location: index.php");
+            } else {
+                $message = "<label>Wrong password</label>";
+            }
+        endforeach;
+    } else {
+        $message = '<label>Wrong username</label>';
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat Application with PHP ajax jQuery</title>
+    <link rel="stylesheet" href="./vendor/jquery-ui/jquery-ui.min.css" type="text/css" />
+    <link rel="stylesheet" href="./vendor/bootstrap.min.css" />
+    <script src="./vendor/bootstrap.min.js" type="text/javascript"></script>
+    <script src="./vendor/jquery-3.3.1.js" type="text/javascript"></script>
+    <script src="./vendor/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <div class="container">
+        <br />
+        <h3 style="text-align:center;">Chat Application with PHP ajax jQuery</h3><br />
+        <div class="panel panel-default">
+            <div class="panel-heading">Chat Application Login</div>
+            <div class="panel-body">
+                <p class="text-danger"><?= $message; ?></p>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label><input type="text" name="username" class="form-control" id="" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label><input type="password" name="password" id="" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Login" class="btn btn-info" name="login">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ update_last_activity.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "database.php";
+
+session_start();
+
+$query = "UPDATE login_details SET last_activity = now() WHERE login_details_id = '" . $_SESSION['login_details_id'] . "'";
+
+$statement =  $connect->prepare($query);
+
+$statement->execute();
